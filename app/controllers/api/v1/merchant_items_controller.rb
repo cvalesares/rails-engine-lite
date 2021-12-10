6c3b45b474c270da85e4153510ecd5751fec9d2e
Christian Valesares <cvalesares@gmail.com>
2021-12-10 18:31:58 -0500

    add sad path test for merchant_items

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -4,7 +4,7 @@ class Api::V1::MerchantItemsController < ApplicationController
     if Item.exists?(merchant_id: params[:merchant_id])
       render json: ItemSerializer.new(Item.where(merchant_id: params[:merchant_id]))
     else
-      render json: { errors: { details: "No merchant matches this id" }}, status: 404
+      render json: { errors: { details: "No items available for this merchant" }}, status: 404
     end
   end
 end
