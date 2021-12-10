2bf8416c18f65eff568ff0ad57e006adf4cbf848
Christian Valesares <cvalesares@gmail.com>
2021-12-10 18:04:46 -0500

    add sad path testing for items search

--- app/controllers/api/v1/items_search_controller.rb
+++ app/controllers/api/v1/items_search_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::ItemsSearchController < ApplicationController
   def find_all
-    if Item.find_all_by_name(params[:name]) == nil || params[:name] == ""
+    # binding.pry
+    if Item.find_all_by_name(params[:name]) == [] || params[:name] == ""
       render json: { data: { details: "No item matches this name" }}, status: 404
     else
       render json: ItemSerializer.new(Item.find_all_by_name(params[:name]))
