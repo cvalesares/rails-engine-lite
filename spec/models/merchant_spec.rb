3b64d20af70d56fd64d75f33350a194f128177fe
Christian Valesares <cvalesares@gmail.com>
2021-12-10 09:46:11 -0500

    add model tests for search methods

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Merchant, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'can find a single merchant by name' do
+    merchant = Merchant.create!(name: "Bob Belcher")
+    merchant2 = Merchant.create!(name: "Rob Berto")
+    merchant3 = Merchant.create!(name: "John Smith")
+    item = merchant.items.create!(name: "sword", description: "pointy", unit_price: 32)
+    item2 = merchant.items.create!(name: "shield", description: "not pointy", unit_price: 30)
+
+    expect(Merchant.find_by_name("ob")).to eq(merchant)
+  end
 end
